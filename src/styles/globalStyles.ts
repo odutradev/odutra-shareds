88506fd35d943684403b044a1cc20d9f76d92e92
odutradev <joao.vitornl@gmail.com>
2026-01-14 15:43:30 -0300

    style: Changing global scroll styles

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,10 +1,34 @@
-import { createGlobalStyle } from "styled-components";
+import { alpha, Theme } from '@mui/material/styles';
+import { createGlobalStyle } from 'styled-components';
 
-const GlobalStyles = createGlobalStyle`
+const GlobalStyles = createGlobalStyle<{ theme: Theme }>`
   *, *::before, *::after {
     box-sizing: border-box;
     margin: 0;
     padding: 0;
+  }
+
+  html {
+    scrollbar-color: ${({ theme }) => alpha(theme.palette.primary.main, 0.2)} transparent;
+    scrollbar-width: thin;
+  }
+
+  ::-webkit-scrollbar {
+    height: 8px;
+    width: 8px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background: transparent;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: ${({ theme }) => alpha(theme.palette.primary.main, 0.2)};
+    border-radius: 4px;
+  }
+
+  ::-webkit-scrollbar-thumb:hover {
+    background-color: ${({ theme }) => alpha(theme.palette.primary.main, 0.4)};
   }
 `;
 
